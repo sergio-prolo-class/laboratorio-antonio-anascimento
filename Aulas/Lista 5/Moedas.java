003472a299b1bfeb2d693d8cbf1418fa103d7b18
antonio-anascimento <antonio.a2007@aluno.ifsc.edu.br>
2025-10-12 19:32:51 +0000

    Exercicio 3 resolvido

---
+++ Aulas/Lista 5/Moedas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Moedas {
+    public static void main (String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um valor em centavos");
+        int valor = sc.nextInt();
+        int moeda = valor/50;
+        valor = valor%50;
+        System.out.println("Moedas de 50: " + moeda);
+        moeda = valor/25;
+        valor = valor%25;
+        System.out.println("Moedas de 25: " + moeda);
+        moeda = valor/10;
+        valor = valor%10;
+        System.out.println("Moedas de 10: " + moeda);
+        moeda = valor/5;
+        valor = valor%5;
+        System.out.println("Moedas de 5: " + moeda);
+        moeda = valor/1;
+        valor = valor%1;
+        System.out.println("Moedas de 1: " + moeda);
+
+    }
+}
